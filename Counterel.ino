3ff3a72355829611b9cb7b2913e197b52e722cbd
Juan Cruz Lepore <jclepore@fb.com>
2020-12-12 14:21:27 +0100

    button blink

--- Counterel.ino
+++ Counterel.ino
@@ -9,67 +9,67 @@
 byte zero[] = //8x5
 {
   B00100,  // digit 0
-   B01010,
-   B10001,
-   B10001,
-   B10001,
-   B10001,
-   B01010,
-   B00100
+  B01010,
+  B10001,
+  B10001,
+  B10001,
+  B10001,
+  B01010,
+  B00100
 };
 
 byte one[] = //8x5
 {
-   B00010,  // digit 1
-   B00110,
-   B01010,
-   B10010,
-   B00001,
-   B00001,
-   B00001,
-   B00001
+  B00010,  // digit 1
+  B00110,
+  B01010,
+  B10010,
+  B00001,
+  B00001,
+  B00001,
+  B00001
 };
 
 byte two[] = //8x5
 {
-   B00100,  // digit 2
-   B01010,
-   B10001,
-   B00001,
-   B00010,
-   B00100,
-   B01000,
-   B11111
+  B00100,  // digit 2
+  B01010,
+  B10001,
+  B00001,
+  B00010,
+  B00100,
+  B01000,
+  B11111
 };
 
 byte three[] = //8x5
 {
-   B01110,  // digit 3
-   B10001,
-   B00001,
-   B00110,
-   B00001,
-   B10001,
-   B01010,
-   B00100
+  B01110,  // digit 3
+  B10001,
+  B00001,
+  B00110,
+  B00001,
+  B10001,
+  B01010,
+  B00100
 };
 
 byte four[] = //8x5
 {
-   B00010,  // digit 4
-   B00110,
-   B01010,
-   B10010,
-   B11111,
-   B00010,
-   B00010,
-   B00010
+  B00010,  // digit 4
+  B00110,
+  B01010,
+  B10010,
+  B11111,
+  B00010,
+  B00010,
+  B00010
 };
 
 
 byte five[] = //8x5
 {
-  B11111,  // 5
+  B11111,  // First frame of invader #1
   B10000,
   B10000,
   B11110,
@@ -81,7 +81,7 @@ byte five[] = //8x5
 
 byte six[] = //8x5
 {
-  B00110,  // 6
+  B00110,  // First frame of invader #1
   B01001,
   B11000,
   B11110,
@@ -93,7 +93,7 @@ byte six[] = //8x5
 
 byte seven[] = //8x5
 {
-  B11111,  // 7
+  B11111,  // First frame of invader #1
   B00001,
   B00010,
   B11111,
@@ -105,7 +105,7 @@ byte seven[] = //8x5
 
 byte eight[] = //8x5
 {
-  B01110,  // 8
+  B01110,  // First frame of invader #1
   B10001,
   B10001,
   B01110,
@@ -117,7 +117,7 @@ byte eight[] = //8x5
 
 byte nine[] = //8x5
 {
-  B01110,  // 9
+  B01110,  // First frame of invader #1
   B10001,
   B10011,
   B01111,
@@ -129,26 +129,34 @@ byte nine[] = //8x5
 
 LedControl lc = LedControl(12, 10, 11, 4); // Pins: DIN,CLK,CS, see Youtube tutorial for details, num displays connected
 
-// Put values in arrays
-
+const int BUTTON_PIN = 7;
 
 void setup() {
-  // put your setup code here, to run once:
-  for(int display_index=0; display_index<4; display_index++) {
-     lc.shutdown(display_index, false); // Wake up displays
-     lc.setIntensity(display_index, 5); // Set intensity levels
-     lc.clearDisplay(display_index);  // Clear Display
-  }
+  // Display setup
+  lc.shutdown(0, false); // Wake up displays
+  lc.setIntensity(0, 5); // Set intensity levels
+  lc.clearDisplay(0);  // Clear Display
+  lc.setLed(0, 1, 1, true);
+
+  // Define pin #07 as input and activate the internal pull-up resistor
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  // Define internal led as output
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-    //lc.setLed(0, 1, 9, true);
+  lc.setLed(0, 1, 1, true);
   // put your main code here, to run repeatedly:
-       for(int index=0;index<4;index++) {
-        lc.setLed(index, 1, 2, true);
-    } 
-}
 
-void showNumber(int number) {
-  
+  // Read the value of the input. It can either be 1 or 0
+  int buttonValue = digitalRead(BUTTON_PIN);
+  if (buttonValue == LOW) {
+    // If button apushed, turn LED on
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    // Otherwise, turn the LED off
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
 }
