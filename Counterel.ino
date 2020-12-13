3171225b9c3bd4cbde61e7b82bc54b967d82e964
Anne Wullenweber <anne@wullenweber-informatik.de>
2020-12-13 20:39:00 +0100

    Implemented ShowNumber

--- Counterel.ino
+++ Counterel.ino
@@ -6,241 +6,208 @@
 
 LedControl lc = LedControl(12, 10, 11, 4); // Pins: DIN,CLK,CS, see Youtube tutorial for details, num displays connected
 
-String no_number[] = 
+int no_number[] = 
 {
-  "00000",
-  "00000",
-  "00000",
-  "00000",
-  "00000",
-  "00000",
-  "00000",
-  "00000"
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000
 };
 
-String zero[] = //8x5
+int zero[] = //8x5
 {
-   "00100",  // digit 0
-   "01010",
-   "10001",
-   "10001",
-   "10001",
-   "10001",
-   "01010",
-   "00100"
+    B01110,  // digit 0
+    B10001,
+    B10001,
+    B10001,
+    B10001,
+    B10001,
+    B10001,
+    B01110
 };
 
-String one[] = //8x5
+int one[] = //8x5
 {
-    "00010",  // digit 1
-    "00110",
-    "01010",
-    "10010",
-    "00010",
-    "00010",
-    "00010",
-    "00010"
+    B00010,  // digit 1
+    B00110,
+    B01010,
+    B10010,
+    B00010,
+    B00010,
+    B00010,
+    B00010
 };
 
-String two[] = //8x5
+int two[] = //8x5
 {
-    "00100",  // digit 2
-    "01010",
-    "10001",
-    "00001",
-    "00010",
-    "00100",
-    "01000",
-    "11111"
+    B01110,  // digit 2
+    B10001,
+    B10001,
+    B00001,
+    B00010,
+    B00100,
+    B01000,
+    B11111
 };
 
-String three[] = //8x5
+int three[] = //8x5
 {
-    "01110",  // digit 3
-    "10001",
-    "00001",
-    "00110",
-    "00001",
-    "10001",
-    "01010",
-    "00100"
+    B01110,  // digit 3
+    B10001,
+    B00001,
+    B00110,
+    B00001,
+    B00001,
+    B10001,
+    B01110
 };
 
-String four[] = //8x5
+int four[] = //8x5
 {
-    "00010",  // digit 4
-    "00110",
-    "01010",
-    "10010",
-    "11111",
-    "00010",
-    "00010",
-    "00010"
+    B00010,  // digit 4
+    B00110,
+    B01010,
+    B10010,
+    B11111,
+    B00010,
+    B00010,
+    B00010
 };
 
 
-String five[] = //8x5
+int five[] = //8x5
 {
-    "11111",  // 5
-    "10000",
-    "10000",
-    "11110",
-    "00011",
-    "00001",
-    "00011",
-    "11110"
+    B11111,  // 5
+    B10000,
+    B10000,
+    B11110,
+    B00011,
+    B00001,
+    B00011,
+    B11110
 };
 
-String six[] = //8x5
+int six[] = //8x5
 {
-    "00110",  // 6
-    "01001",
-    "11000",
-    "11110",
-    "10011",
-    "10001",
-    "11011",
-    "01110"
+    B00110,  // 6
+    B01001,
+    B11000,
+    B11110,
+    B10011,
+    B10001,
+    B11011,
+    B01110
 };
 
-String seven[] = //8x5
+int seven[] = //8x5
 {
-    "11111",  // 7
-    "00001",
-    "00010",
-    "11111",
-    "01000",
-    "01000",
-    "10000",
-    "10000"
+    B11111,  // 7
+    B00001,
+    B00010,
+    B11111,
+    B01000,
+    B01000,
+    B10000,
+    B10000
 };
 
-String eight[] = //8x5
+int eight[] = //8x5
 {
-    "01110",  // 8
-    "10001",
-    "10001",
-    "01110",
-    "10011",
-    "10001",
-    "10001",
-    "01110"
+    B01110,  // 8
+    B10001,
+    B10001,
+    B01110,
+    B10011,
+    B10001,
+    B10001,
+    B01110
 };
 
-String nine[] = //8x5
+int nine[] = //8x5
 {
-    "01110",  // 9
-    "10001",
-    "10011",
-    "01111",
-    "00001",
-    "00001",
-    "10001",
-    "01110"
+    B01110,  // 9
+    B10001,
+    B10011,
+    B01111,
+    B00001,
+    B00001,
+    B10001,
+    B01110
 };
 
-String * getSegment(char character){
+int * getSegment(char character){
     switch(character) {
-        case ' ': return no_number;  break;
-        //case '0': return zero; break; 
+        case '.': return no_number;  break;
+        case '0': return zero; break; 
         case '1': return one; break; 
-        //case '2': return two; break; 
-        //case '3': return three; break; 
-        //case '4': return four; break; 
-        //case '5': return five; break; 
-        //case '6': return six; break; 
-        //case '7': return seven; break; 
-        //case '8': return eight; break; 
-        //case '9': return nine; break; 
+        case '2': return two; break; 
+        case '3': return three; break; 
+        case '4': return four; break; 
+        case '5': return five; break; 
+        case '6': return six; break; 
+        case '7': return seven; break; 
+        case '8': return eight; break; 
+        case '9': return nine; break; 
         default: return no_number; break;
   }
 }
 
-byte stringToByte(String number){
-  byte result = 0;
-  byte multiplier = 1;
-  for (int i = 7; i>=0 ; i--){
-    result += (number[i]-'0')  * multiplier;
-    multiplier *= 2;
-  }
-  return result;
-}
-
 void showNumber(int mynum) {
-    String s_num = String(mynum);
-    //while(s_num.length() < 5) {
-      //String converted_num;  
-     // converted_num += ' ';
-      //s_num = converted_num.concat(s_num);
-    //}
-    String combined_arr [8][5];
+    char num_str[5];
+    itoa(mynum, num_str, 10);
+    int divide = 10000;
+    while(mynum / divide < 1) {  
+        num_str[4] = num_str[3];
+        num_str[3] = num_str[2];
+        num_str[2] = num_str[1];
+        num_str[1] = num_str[0];
+        num_str[0] =  '.';
+        divide = divide / 10;
+    }
+    int combined_arr [8][5];
     for (int digit = 0; digit < 5; digit++){
-      Serial.print("New digit\n");
         for (int i = 0; i < 8; i++){
-            combined_arr[i][digit] = getSegment(s_num.charAt(digit))[i];
-            Serial.print(combined_arr[i][digit]);
-            Serial.print("\n");
+            combined_arr[i][digit] = getSegment(num_str[digit])[i];
         }
     }
 
-    //segment 3: Ziffer1(1-5) leer(1) Ziffer2(1-2)
-    String str_segment3 [8];
+    int segment3 [8];
     for (int i = 0; i < 8; i++) {
-        str_segment3[i] = combined_arr[i][1].charAt(0);
-        str_segment3[i] += combined_arr[i][1].charAt(1);
-        str_segment3[i] += combined_arr[i][1].charAt(2);
-        str_segment3[i] += combined_arr[i][1].charAt(3);
-        str_segment3[i] += combined_arr[i][1].charAt(4);
-        str_segment3[i] += '0';
-        str_segment3[i] += combined_arr[i][2].charAt(0);
-        str_segment3[i] += combined_arr[i][2].charAt(1);
+        segment3[i] = combined_arr[i][0] << 3; //digit 1 + one empty line
+        segment3[i] += combined_arr[i][1] >> 3; //shifted to the right
+     }
+
+    int segment2 [8];
+    for (int i = 0; i < 8; i++) {
+        segment2[i] = (combined_arr[i][1] & B00111) << 5; //last three digits of digit 2  + 3 empty lines 
+        segment2[i] += (combined_arr[i][2] >> 3); //two digits of digit three 
     }
-    //segment 2: Ziffer2(3-5) leer(1) Punkt(1) leer(1) Ziffer3(1-2)
-    String str_segment2 [8];
-    for (int i = 0; i < 8; i++) {
-        str_segment2[i] = combined_arr[i][2].charAt(2);
-        str_segment2[i] += combined_arr[i][2].charAt(3);
-        str_segment2[i] += combined_arr[i][2].charAt(4);
-        str_segment2[i] += '0';
-        str_segment2[i] += '0';
-        str_segment2[i] += '0';
-        str_segment2[i] += combined_arr[i][3].charAt(0);
-        str_segment2[i] += combined_arr[i][3].charAt(1);
+    if (mynum >= 1000){
+      segment2[7] += 8; //add point
     }
-    str_segment2[7][4] = '1';
-    //segment 1: Ziffer3(3-5) leer(1) Ziffer 4(1-4)
-    String str_segment1 [8];
+
+    int segment1 [8];
     for (int i = 0; i < 8; i++) {
-        str_segment1[i] = combined_arr[i][3].charAt(2);
-        str_segment1[i] += combined_arr[i][3].charAt(3);
-        str_segment1[i] += combined_arr[i][3].charAt(4);
-        str_segment1[i] += '0';
-        str_segment1[i] += combined_arr[i][4].charAt(0);
-        str_segment1[i] += combined_arr[i][4].charAt(1);
-        str_segment1[i] += combined_arr[i][4].charAt(2);
-        str_segment1[i] += combined_arr[i][4].charAt(3);
+        segment1[i] = (combined_arr[i][2] & B00111) << 5; //three digits of three + empty line 
+        segment1[i] += (combined_arr[i][3] >> 1); //4 digits of four
     }
-    //segment 0: Ziffer4(5) leer(1) Ziffer5(1-5) leer(1)
-    String str_segment0 [8];
+
+    int segment0 [8];
     for (int i = 0; i < 8; i++) {
-        str_segment0[i] = combined_arr[i][4].charAt(4);
-        str_segment0[i] += '0';
-        str_segment0[i] += combined_arr[i][5].charAt(0);
-        str_segment0[i] += combined_arr[i][5].charAt(1);
-        str_segment0[i] += combined_arr[i][5].charAt(2);
-        str_segment0[i] += combined_arr[i][5].charAt(3);
-        str_segment0[i] += combined_arr[i][5].charAt(4);
-        str_segment0[i] += '0';
-        Serial.print(str_segment0[i]);
-        Serial.print("\n");
+        segment0[i] = (combined_arr[i][3] & B00001) << 7; //last digit of four + empty line
+        segment0[i] += combined_arr[i][4] << 1;
     }
 
   for (int i = 0; i < 8; i++)
   {
-    lc.setRow(0,i, stringToByte(str_segment0[i]));
-    lc.setRow(1,i, stringToByte(str_segment1[i]));
-    lc.setRow(2,i, stringToByte(str_segment2[i]));
-    lc.setRow(3,i, stringToByte(str_segment3[i]));
+    lc.setRow(0,i, segment0[i]);
+    lc.setRow(1,i, segment1[i]);
+    lc.setRow(2,i, segment2[i]);
+    lc.setRow(3,i, segment3[i]);
   }
 }
 
@@ -254,18 +221,9 @@ void setup() {
      lc.setIntensity(display_index, 5); // Set intensity levels
      lc.clearDisplay(display_index);  // Clear Display
   }
-  Serial.begin(9600);
-  int test_num = 11111;
+  int test_num = 6372;
   showNumber(test_num);
-  //for (int i = 0; i < 8; i++){
-    //Serial.print(getSegment('1')[i]);
-      //      Serial.print("\n");
-        //}
 }
 
 void loop() {
-    //lc.setLed(0, 1, 9, true);
-  // put your main code here, to run repecharAtedly:
-  //String test = "00001001";
-  //lc.setRow(3,1, stringToByte(test));
 }
